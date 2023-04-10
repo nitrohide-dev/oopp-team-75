5016417a7a18ab0a4bc003f6cd7dcd714712b20e
Anonymous <anonymous@example.com>
2023-04-10 13:46:52 +0200

    cehckstyle

--- commons/src/test/java/commons/TagTest.java
+++ commons/src/test/java/commons/TagTest.java
@@ -6,7 +6,12 @@ import org.junit.jupiter.api.Test;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TagTest {
 
